b5f90ce55c2243569ab59fc5785af81500e25190
lii0930 <phuongplups25732@fpt.edu.vn>
2024-09-16 15:25:45 +0700

    first commit

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+
+import {
+  RiGithubLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiBehanceLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiBehanceLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,43 @@
+// import {} from "react-icons/rx";
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
+              My Services <span className="text-accent">.</span>
+            </motion.h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0 ">
+              Voluptate non tempor exercitation sunt commodo dolor anim
+              reprehenderit dolor do occaecat excepteur minim. Ad pariatur
+              mollit quis elit. Pariatur elit id laborum ullamco voluptate
+              reprehenderit quis et veniam Lorem velit. Laboris officia mollit
+              do nisi commodo ullamco adipisicing.
+            </p>
+          </div>
+          <div className="w-full xl:max-w-[65%]">
+            {/* slider */}
+            <ServiceSlider />
+          </div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
